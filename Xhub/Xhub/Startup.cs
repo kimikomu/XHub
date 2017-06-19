b15576e0a3c0353291fdd706e92e77882717c04b
kimikomu <kimikomu@gmail.com>
2017-06-19 12:17:55 -0700

    Initial commit

---
+++ Xhub/Xhub/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Xhub.Startup))]
+namespace Xhub
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
