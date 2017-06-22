3c317c4318959034846929bdcabf5aa91cf8ba88
kimikomu <kimikomu@gmail.com>
2017-06-21 17:22:35 -0700

    Create  Add an Event form. Add a lang attribute to _Layout html.

---
+++ Xhub/Xhub/Controllers/EventsController.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using Xhub.Models;
+using Xhub.ViewModels;
+
+namespace Xhub.Controllers
+{
+	public class EventsController : Controller
+	{
+		// Set up the DbContext
+		private readonly ApplicationDbContext _context;
+
+		public EventsController()
+		{
+			_context = new ApplicationDbContext();
+		}
+
+		// Action methods
+		public ActionResult Add()
+		{
+			// Create a view model with the list of event types to return
+			var viewModel = new EventsFormViewModel
+			{
+				EventTypes = _context.EventTypes.ToList()
+			};
+
+			return View(viewModel);
+		}
+	}
+}
---
+++ Xhub/Xhub/ViewModels/EventsFormViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Xhub.Models;
+
+namespace Xhub.ViewModels
+{
+	public class EventsFormViewModel
+	{
+		public string EventLocation { get; set; }
+		public string Date { get; set; }
+		public string Time { get; set; }
+		public int EventType { get; set; }
+		public IEnumerable<EventType> EventTypes { get; set; }
+	}
+}
