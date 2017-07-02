623614905879cb4a130f5005753e81b3b5e01947
kimikomu <kimikomu@gmail.com>
2017-07-02 11:20:35 -0700

    Add name to User.

---
+++ Xhub/Xhub/Models/Student.cs
@@ -0,0 +1,19 @@
+﻿namespace Xhub.Models
+{
+	public class Student
+	{
+		public int Id { get; set; }
+
+		public string Alias { get; set; }
+		public string Ability { get; set; }
+		public int Grade { get; set; }
+
+		public enum Origin
+		{
+			Special,
+			Mutant,
+			Alein,
+			Other
+		}
+	}
+}
